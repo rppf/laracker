1976346e7714a66b37aeb0c261e5228bebdda6c7
Raymond <raymondflorendo.320@gmail.com>
2018-06-04 09:27:26 +0800

    add -e on setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Downloading dependencies \e[1mBold"
+echo -e "Downloading dependencies \e[1mBold"
 composer install
 
-echo "Set environment file \e[1mBold"
+echo -e "Set environment file \e[1mBold"
 cp .env.example .env;php artisan key:generate
 
-echo "Set file permissions \e[1mBold"
+echo -e "Set file permissions \e[1mBold"
 chown -R www-data:www-data /var/www/
 
-echo "Success \e[1mBold \e[32mGreen"
+echo -e "Success \e[1mBold \e[32mGreen"
